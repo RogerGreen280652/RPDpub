1d90e257f2849e4e14c42c81d1735908d398ed3e
Romaha <rogergreen280652@gmail.com>
2021-07-03 13:40:04 +0800

    Сервак заработал, исправил урлы. Чтобы посмотреть результат, нужно перейти по следующей ссылке: http://127.0.0.1:8000/DocumentCreator/

--- rpd/urls.py
+++ rpd/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('rpd/', include('rpd.urls')),
+    path('DocumentCreator/', include('DocumentCreator.urls')),
     path('admin/', admin.site.urls),
 ]
